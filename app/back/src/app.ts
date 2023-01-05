d1b89369a1df53d7597b4bbe2dd2559c1c48e4d6
hector <hectoreddbenitez@gmail.com>
2023-01-05 18:43:30 -0300

    feat(App): modifica rotas

--- app/back/src/app.ts
+++ app/back/src/app.ts
@@ -1,8 +1,13 @@
-import express, { json } from 'express';
+import express from 'express';
+import accountsRouter from './routes/AccountsRoutes';
+import transactionsRouter from './routes/TransactionsRoutes';
 import usersRouter from './routes/UserRoutes';
 
 const app = express();
 
-app.use(json());
-app.use(usersRouter)
+app.use(express.json());
+app.use(usersRouter);
+app.use(transactionsRouter);
+app.use(accountsRouter);
+
 export default app;
