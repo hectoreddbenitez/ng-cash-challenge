f3e00b4bc693acb98ac62ef9364c8a8de945d775
hector <hectoreddbenitez@gmail.com>
2023-01-09 18:58:00 -0300

    style(AccountService): melhora formatação

--- app/back/src/services/AccountsService.ts
+++ app/back/src/services/AccountsService.ts
@@ -1,7 +1,7 @@
 import AccountsModel from '../database/models/AccountsModel';
 import ErrorCode from '../interfaces/Error';
-import {Sequelize} from 'sequelize';
-import config from '../database/config/database'
+import { Sequelize } from 'sequelize';
+import config from '../database/config/database';
 
 const sequelize = new Sequelize(config);
 
@@ -20,7 +20,7 @@ export default class AccountsService {
     console.log(id);
     const account = await AccountsModel.findOne({ where: { id } });
     console.log(account, 'aqui el account');
-    
+
     if (!account) {
       const errorPayload = new ErrorCode('conta inexistente ', 401);
       return errorPayload;
@@ -28,33 +28,37 @@ export default class AccountsService {
     return account;
   }
 
-  static async updaterAccount(accountOutId: number, accountInnId: number,value: number) {
-    try{
+  static async updaterAccount(
+    accountOutId: number,
+    accountInnId: number,
+    value: number
+  ) {
+    try {
       const accountOut = (await AccountsModel.findOne({
         where: { id: accountOutId },
       })) as AccountsModel;
-      const newValue1 = accountOut.balance - value;
+      const newValueAccountOut = accountOut.balance - value;
 
       const accountInn = (await AccountsModel.findOne({
         where: { id: accountInnId },
       })) as AccountsModel;
-      const newValue2 = accountInn.balance + value;
+      const newValueAccountInn = accountInn.balance + value;
 
-      const result = await sequelize.transaction(async (t) => {
+      const moneyTransferResult = await sequelize.transaction(async (t) => {
         await AccountsModel.update(
-          { value: newValue1 },
-          { where: { id: accountOutId },
-         transaction: t });
+          { value: newValueAccountOut },
+          { where: { id: accountOutId }, transaction: t }
+        );
 
-         await AccountsModel.update(
-          { value: newValue2 },
-          { where: { id: accountInnId },
-         transaction: t });
-    });
-    return result;
-  } catch (e) {
-    console.log(e);
-    throw e;
+        await AccountsModel.update(
+          { value: newValueAccountInn },
+          { where: { id: accountInnId }, transaction: t }
+        );
+      });
+      return moneyTransferResult;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   }
-}
 }
